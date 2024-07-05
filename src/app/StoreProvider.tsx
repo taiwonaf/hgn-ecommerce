dd414d5022d7896bd0e598bf541bd2ca900c92b7
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2024-07-05 05:25:48 -0700

    fix: Up providers in a separate file

---
+++ src/app/StoreProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { persistor, store } from "@/redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+interface IPropTypes {
+  children: React.ReactNode;
+}
+
+const StoreProvider: React.FC<IPropTypes> = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default StoreProvider;
